9c3478e76edf0a63386fcbffd2f723580b912008
Gurkan <hello@gurkz.me>
2026-02-13 21:10:41 +0100

    datagen (woohoo)

---
+++ src/client/java/test/testerer/datagen/ModModelProvider.java
@@ -0,0 +1,28 @@
+package test.testerer.datagen;
+
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
+import test.testerer.block.ModBlocks;
+
+public class ModModelProvider extends FabricModelProvider {
+    public ModModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockModelGenerators generator) {
+        generator.createTrivialCube(ModBlocks.C4);
+        generator.createTrivialCube(ModBlocks.TESTERER_BLOCK_YAY);
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerators generator) {
+    }
+
+    @Override
+    public String getName() {
+        return "ModModelProvider";
+    }
+}
