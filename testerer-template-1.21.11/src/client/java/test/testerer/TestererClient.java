ff6ac90109f6215a2bd98a5dacbddde0538f2db8
miniblocks2123 <109236749+miniblocks2123@users.noreply.github.com>
2026-02-13 18:15:25 +0000

    because mod
    because mod

---
+++ testerer-template-1.21.11/src/client/java/test/testerer/TestererClient.java
@@ -0,0 +1,10 @@
+package test.testerer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestererClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
