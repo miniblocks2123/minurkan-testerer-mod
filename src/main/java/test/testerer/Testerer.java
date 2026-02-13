0aa7587bbe100ef284d58d3f912bcf107c9bc907
Gurkan <hello@gurkz.me>
2026-02-13 20:00:08 +0100

    use parchmentmc

--- src/main/java/test/testerer/Testerer.java
+++ src/main/java/test/testerer/Testerer.java
@@ -19,6 +19,6 @@ public class Testerer implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("test black colour");
 	}
 }
